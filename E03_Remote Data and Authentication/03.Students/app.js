26830b0491ee66f678a8b4c492758bbb7b706b87
Nelly Vladova <101873582+NellyVladova@users.noreply.github.com>
2022-07-09 11:50:54 +0300

    Update app.js

--- E03_Remote Data and Authentication/03.Students/app.js
+++ E03_Remote Data and Authentication/03.Students/app.js
@@ -1,17 +1,43 @@
 const url = 'http://localhost:3030/jsonstore/collections/students';
-const infoTable = document.querySelector('#result tbody');
-const formInput = document.querySelector('form');
+
+const studentsTable = document.querySelector('#results tbody');
+const inputForm = document.querySelector('form');
+const submitBtn = document.getElementById('submit');
 
 window.addEventListener('load', loadStudents);
-document.getElementById('submit').addEventListener('click', addNewStudent);
+submitBtn.addEventListener('click', addNewStudent);
+
+async function loadStudents() {
+    try {
+        const response = await fetch(url);
+
+        if (response.status != 200) {
+            throw new Error("Can't fetch data!");
+        }
+
+        const data = await response.json();
+
+        Object.values(data).forEach(el => {
+            const newStudent = createNewElement('tr',
+                createNewElement('td', el.firstName),
+                createNewElement('td', el.lastName),
+                createNewElement('td', el.facultyNumber),
+                createNewElement('td', el.grade)
+            );
+            studentsTable.appendChild(newStudent);
+        })
+    } catch (error) {
+        alert(error.message);
+    }
+}
 
 async function addNewStudent(event) {
     event.preventDefault();
 
-    const formData = new FormData(formInput);
-    const studentInfo = [...formData.values];
+    const formData = new FormData(inputForm);
+    const studentInfo = [...formData.values()];
 
-    infoTable.replaceChildren();
+    studentsTable.replaceChildren();
 
     try {
         const response = await fetch(url, {
@@ -23,40 +49,15 @@ async function addNewStudent(event) {
                 firstName: studentInfo[0],
                 lastName: studentInfo[1],
                 facultyNumber: studentInfo[2],
-                grade: Number(studentInfo[3])
+                grade: Number(studentInfo[3].trim())
             })
         });
 
         if (response.status != 200) {
-            throw new Error("Can't create new student!");
+            throw new Error("Can't create new student record!");
         }
 
-        loadStudents();
-    } catch (error) {
-        alert(error.message);
-    }
-}
-
-async function loadStudents() {
-    try {
-        const response = await fetch(url);
-
-        if (response.status != 200) {
-            throw new Error("Can't fetch data!");
-        }
-        const data = await response.json();
-
-        Object.values(data).forEach(el => {
-            const newStudent = createNewElement('tr',
-                createNewElement('td', el.firstName),
-                createNewElement('td', el.lastName),
-                createNewElement('td', el.facultyNumber),
-                createNewElement('td', el.grade)
-            );
-
-            infoTable.appendChild(newStudent);
-        });
-
+        removeContent();
         loadStudents();
     } catch (error) {
         alert(error.message);
@@ -67,12 +68,18 @@ function createNewElement(type, ...content) {
     const newEl = document.createElement(type);
 
     content.forEach(el => {
-        if (typeof el == 'string' || typeof el === 'number') {
+        if (typeof el === 'number' || typeof el == 'string') {
             el = document.createTextNode(el);
         }
-
         newEl.appendChild(el);
     })
 
     return newEl;
+}
+
+function removeContent(){
+    const inputParent = document.querySelector('.inputs');
+    inputParent.childNodes.forEach(child => {
+        child.value = '';
+    })
 }
